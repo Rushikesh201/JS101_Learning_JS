eeb633cf886daa70817c8145d30f9dd9c74a8f03
Rushi <deshmukhrushikesh511@gmail.com>
2023-04-22 20:30:16 +0530

    print-masai-school

---
+++ program 1.js
@@ -0,0 +1,10 @@
+//print masai scholl
+//print A trasformation in Education
+
+let A="Masai Scholl";
+
+let B="A Transformation in Education";
+
+console.log(A);
+
+console.log(B);
---
+++ program 3.js
@@ -0,0 +1,8 @@
+// print name & ege
+let name="Rushi";
+
+let age="26";
+
+console.log(name,age);
+
+console.log(typeof(name),typeof(age));
